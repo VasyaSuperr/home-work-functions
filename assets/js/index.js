9dd386addbcbe38e48c76bfd579cc46f9ac00d23
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-22 21:17:19 +0300

    task 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,3 +31,26 @@ console.log(
   `Число ${fisrtNumber} ділиться націло на число ${secondNumber} :>> `,
   checkMultiplicity(fisrtNumber, secondNumber)
 );
+
+console.log("Завдання 3");
+/**
+ * @function isThereTriangle
+ * @param {number} AB - side AB a triangle
+ * @param {number} BC - side BC a triangle
+ * @param {number} AC - side AC a triangle
+ * @returns {boolean} A boolean value that indicates whether a triangle exists or not
+ */
+function isThereTriangle(AB, BC, AC) {
+  return (
+    AB > 0 && BC > 0 && AC > 0 && AB + BC > AC && AB + AC > BC && AC + BC > AB
+  );
+}
+
+const sideAB = 3;
+const sideBC = 6;
+const sideAC = 5;
+
+console.log(
+  `Існує трикутник з такими сторонами ${sideAB}, ${sideBC}, ${sideAC} :>> `,
+  isThereTriangle(sideAB, sideBC, sideAC)
+);
