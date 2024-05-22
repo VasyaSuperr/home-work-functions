50331f353fc885dea6f53c23f4f7186b689c0ad3
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-22 19:13:38 +0300

    task 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,13 @@
+console.log("Завдання 1");
 
+/**
+ * @function isWorkingAgePerson
+ * @param {number} agePeson - Age of a person
+ * @returns {boolean} A boolean indicating if the person is of working age (able-bodied person or not)
+ */
+function isWorkingAgePerson(agePerson) {
+  return agePerson >= 16 && agePerson <= 64;
+}
+
+const age = 88;
+console.log("Працездатний вік :>> ", isWorkingAgePerson(age));
